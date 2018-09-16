1be792c0a2792098c356d9d1008a65a8945c5441
lbarret4 <llewellynbar4@gmail.com>
2018-09-16 14:05:15 -0400

    Copied chirper api form previous lab and refactored to ES6

---
+++ server/src/routes/chirps.js
@@ -0,0 +1,41 @@
+import {Router} from 'express';
+import chirpsStore from '../chirpstore';
+
+let router = Router();
+
+router.get('/:id?', (req,res) => {
+    let id = req.params.id;
+    if(id === "nextid" ){
+        res.json(chirpsStore.GetChirps().nextid);
+    }else if(id){
+        res.json(chirpsStore.GetChirps(id))
+    }
+    else{
+        res.send(chirpsStore.GetChirps());
+    }
+});
+
+router.post('/',(req,res) => {
+    let previousId= chirpsStore.GetChirps().nextid
+    chirpsStore.CreateChirp(req.body);
+    res.json(previousId);
+   
+
+});
+
+router.put('/:id',(req,res) => {
+    let id = req.params.id;
+    chirpsStore.UpdateChirp(id,req.body);
+    res.sendStatus(200);
+});
+
+router.delete('/:id',(req,res) => {
+    let id = req.params.id;
+    chirpsStore.DeleteChirp(id);
+    res.sendStatus(200);
+});
+
+
+
+module.exports = router;
+
