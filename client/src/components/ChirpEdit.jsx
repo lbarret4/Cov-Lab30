94aebf7e6dcedebd856c6ec17b946fc8b2b81a6e
lbarret4 <llewellynbar4@gmail.com>
2018-09-17 14:24:52 -0400

    added functionality to close button and   save button

--- client/src/components/ChirpEdit.jsx
+++ client/src/components/ChirpEdit.jsx
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import Header from './Header';
+import {Redirect} from 'react-router-dom';
 
 class ChirpEdit extends Component {
     constructor(props) {
@@ -10,9 +11,12 @@ class ChirpEdit extends Component {
                 time:"",
                 user:"",
                 content:""
-            }
+            },
+            redirect:false
         }
         this.handlesEditChirp =this.handlesEditChirp.bind(this);
+        this.handlesSave = this.handlesSave.bind(this);
+        this.handlesClose = this.handlesClose.bind(this)
     }
     async componentDidMount() {
         let id =this.props.match.params.id;
@@ -36,25 +40,72 @@ class ChirpEdit extends Component {
         newChirp.content= e.target.value;
         this.setState({
             chirp:newChirp
+            
                 
 
         });
 
     }
 
+    handlesSave(e){        
+        e.preventDefault();
+        if (this.state.chirp.content.length > 0) {
+            let url = `http://localhost:3000/api/chirps/${this.state.chirp.index}`;
+            let chirp = {};
+            chirp.time = this.state.chirp.time;
+            chirp.user = this.state.chirp.user;
+            chirp.content = this.state.chirp.content;
+            let options = {
+                method: 'PUT',
+                body: JSON.stringify(chirp),
+                headers: {
+                    'Content-Type':'application/json'
+                },
+            };
+            (async (chirp) => {
+                try {
+                    let results = await fetch(url,options);
+                    this.setState({
+                        redirect:true
+                    })
+                    
+
+                } catch (error) {
+                    console.log(error);
+                }
+            })();
+
+
+
+        } else {
+            return alert("\t\t\tEmpty Chirp:\n\nAdd a message before posting Chirps!");
+        }
+
+
+    }
+
+    handlesClose(e){
+        e.preventDefault();
+        this.setState({
+            redirect:true
+        })
+    }
+
 
     render() {
+        let redirect = <Redirect to='/'/>;
         let chirp = this.state.chirp;
         return (
             <Fragment>
-                <Header />
+                {this.state.redirect ? redirect: <Header /> }
+                
                 <div className='model-open'>
                     <div className="modal" role="dialog" aria-labelledby="chirpsModalLabel" style={{ display: 'block' }}>
                         <div className="modal-dialog modal-dialog-centered" role="document">
                             <div className="modal-content">
                                 <div className="modal-header">
                                     <h5 className="modal-title">Edit Chirp</h5>
-                                    <button type="button" className="close">
+                                    <button type="button" className="close" onClick={this.handlesClose}>
                                         <span ariaHidden="true">&times;</span>
                                     </button>
                                 </div>
@@ -63,7 +114,7 @@ class ChirpEdit extends Component {
                                 </div>
                                 <div className="modal-footer">
 
-                                    <button type="button" className="btn btn-primary" >Save changes</button>
+                                    <button type="button" className="btn btn-primary" onClick={this.handlesSave} >Save changes</button>
                                 </div>
                             </div>
                         </div>
