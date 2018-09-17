c2a27ae05faf5747ff62bdcb564334f1d3b5e065
lbarret4 <llewellynbar4@gmail.com>
2018-09-17 17:17:31 -0400

    Add button that is conditionally render base upon withoutButton's value

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+const Header = (props) => {
+    let link = <Link className="btn btn-dark" to="/">Go Home</Link>;
+    return (
+        <Fragment>
+            <h1 className="text-center">Welcome to Chirper !</h1>
+            {props.withoutButton ? props.withoutButton : link}
+        </Fragment>
 
-const Header = () => {
-    return <h1 className="text-center">Welcome to Chirper !</h1>;
+    );
 
 }
 
