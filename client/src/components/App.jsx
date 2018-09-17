3e100bf62248304881a2e871805f11e9ece6f2fd
lbarret4 <llewellynbar4@gmail.com>
2018-09-17 17:17:41 -0400

    Updated import statement due to reorganization

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import Home from './ChirpsFeed';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import Header from './Header';
-import Chirp from './Chirp';
-import ChirpEdit from './ChirpEdit'
+import { Chirp } from './Data';
+import { ChirpEdit, ChirpsFeed as Home } from './View';
 
 const Navigation = () => {
 
@@ -12,10 +10,10 @@ const Navigation = () => {
     <Router>
       <div className="container">
         <Switch>
-            <Route exact path = "/" component={Home} />
-             <Route exact path = "/chirps/:id/edit" component ={ChirpEdit}/>
-            <Route path = "/chirps/:id" component ={Chirp}/> 
-            
+          <Route exact path="/" component={Home} />
+          <Route exact path="/chirps/:id/edit" component={ChirpEdit} />
+          <Route path="/chirps/:id" component={Chirp} />
+
         </Switch>
 
       </div>
