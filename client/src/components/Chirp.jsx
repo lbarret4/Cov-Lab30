a77096f5e643420cbc647e999b874aa275980399
lbarret4 <llewellynbar4@gmail.com>
2018-09-17 11:43:37 -0400

    Refactored into a single chirp and moved card component into ChirpCard

--- client/src/components/Chirp.jsx
+++ client/src/components/Chirp.jsx
@@ -1,37 +1,54 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
+import ChirpCard from './ChirpCard';
+import Header from './Header';
 
-
-
-const Chirp = (props) => {
-
-    let chirp = props.chirp;
-    let timeStyle = {
-        position: "relative",
-        bottom: "1em"
+class Chirp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            chirp:{
+                time:"",
+                user:"",
+                content:""
+            }
+        }
     }
-    return (
-        <Fragment>
-            <div className="d-inline-flex flex-row flex-nowrap">
-                <li className="list-group-item flex-grow-1">
 
-                    <small className="d-block" style={timeStyle}>
-                        {chirp.user} on {chirp.time.toLocaleString()}
-                    </small>
-                    <span className="ml-5">{chirp.content}</span>
+    async componentDidMount() {
+        let url = `http://localhost:3000/api${this.props.match.url}`;
+        try {
+            let results = await fetch(url);
+            let data = await results.json();
+            data.time=new Date(await data.time);
+            this.setState({
+                chirp: data
 
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-                </li>
-                <div className="  list-group-item d-flex align-items-center">
-                    <button type="button" className="close deleteChirp " aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    render() {
+        let chirp =this.state.chirp;
+        console.log(chirp);
+        return (
+            <Fragment>
+                <Header />
+                <div className="card mt-5" style={{ maxWidth: " 90vw" }} >
+
+                    <div className="card-header d-flex justify-content-between">
+                        <span> You can edit chirp or delete it.</span>
+                        <button type="button" className=" btn btn-primary" aria-label="Edit">Edit</button>
+                    </div>
+                    <ChirpCard chirp={chirp} isFeed={false} key={chirp.time} />
                 </div>
-
-            </div>
-
-        </Fragment>
-    );
-
-
+                
+            </Fragment>
+              );
+                }
+                  
 
 }
 
-export default Chirp;
+export default Chirp
