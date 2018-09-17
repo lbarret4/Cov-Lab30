0185cadadfb8fd17ee5ab842f441454a54e79378
lbarret4 <llewellynbar4@gmail.com>
2018-09-16 23:33:30 -0400

    updated names for renamed component and added fetch to get data

--- client/src/components/ChirpsFeed.jsx
+++ client/src/components/ChirpsFeed.jsx
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import ChirpList from './ChirpList';
+import React, { Component, Fragment } from 'react';
+import Chirps from './Chirps';
+import Header from './Header';
 
 
 
@@ -17,32 +18,23 @@ class ChirpsFeed extends Component {
         this.handlesPost = this.handlesPost.bind(this);
     }
 
-    componentDidMount() {
-        const chirp = [
-            {
-                time: new Date(2018, 7, 27, 7, 0),
-                user: "Taomgirl",
-                content: "I woke up really early this morning"
-            },
-            {
-                time: new Date(2018, 7, 28, 7, 11),
-                user: "Divi",
-                content: "Somewhere over the rainbow"
-            },
-            {
-                time: new Date(2018, 7, 29, 23, 0),
-                user: "Bobby Z",
-                content: "Who ate my cheese?"
-            }
+    async componentDidMount() {
+        let url = `http://localhost:3000/api/chirps`;
+        try {
+            let results = await fetch(url);
+            let chirps = await results.json();
+            console.log(chirps);
+            this.setState({
+                chirpList: chirps
 
-        ]
-
-        this.setState({
-            chirpList: chirp
-
-
-        })
+            });
+        } catch (error) {
+            console.log(error);
+        }
     }
+
+
+
 
     handlesNewChirp(e) {
         this.setState({
@@ -78,21 +70,27 @@ class ChirpsFeed extends Component {
     render() {
 
         return (
-            <div className="card mt-5" style={{ maxWidth: " 90vw" }} >
+            <Fragment>
+                <Header />
+                <div className="card mt-5" style={{ maxWidth: " 90vw" }} >
 
-                <div className="card-header">
-                    To get started, type a message and post a chirp.
-                </div>
-                <div className="input-group">
-                    <textarea className="form-control" required onChange={this.handlesNewChirp} value={this.state.chirpContent} />
-                    <div className="input-group-append">
-                        <button type="button" className=" btn btn-primary" onClick={this.handlesPost} >Post</button>
+                    <div className="card-header">
+                        To get started, type a message and post a chirp.
                     </div>
+                    <form className="" action="" method="POST">
+                        <div className="input-group">
+                            <textarea className="form-control" required onChange={this.handlesNewChirp} value={this.state.chirpContent} />
+                            <div className="input-group-append">
+                                <button type="button" className=" btn btn-primary" onClick={this.handlesPost} >Post</button>
+                            </div>
+                        </div>
+                    </form>
+
+
+                    <Chirps chirps={this.state.chirpList} />
+
                 </div>
-
-                <ChirpList chirps={this.state.chirpList} />
-
-            </div>
+            </Fragment>
 
         );
     }
